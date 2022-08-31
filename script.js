b7d1c39b47cec1a8616c7b2642c3774a1aa3b36b
peter-mourad <pmourad772@gmail.com>
2022-08-31 14:38:50 +0200

    added slider feature and tabs functionallity

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000/courses';
+const url = 'http://localhost:3000/Courses';
 const loadData = async () => {
     const res = await fetch(url);
     const data = await res.json();
@@ -7,48 +7,111 @@ const loadData = async () => {
 
 function rateHtmlFormatting(rate){
     let html = `<span class="rate"><strong>${rate}</strong></span>\n`
-    while (rate >= 0) {
-        html += `\t\t\t\t\t\t\t<span class="material-symbols-outlined">${rate >= 1 ? 'star' : 'star_half'}</span>\n`;
-        rate -= 1;
+    for (let i = 0; i < 5; i++, rate--){
+        if (rate >= 1) 
+            html += `\t\t\t\t\t\t\t<i class="fa fa-star"></i>\n`;
+        else if (rate > 0) 
+            html += `\t\t\t\t\t\t\t<i class="fa fa-star-half-full"></i>\n`;
+        else
+            html += `\t\t\t\t\t\t\t<i class="fa fa-star-o"></i>\n`;
     }
     return html;
 }
 
 const getCourse = (data) => {
-    let html = ``;
-    html += `<li class="course-item">
-                <a href="${data.link}" target="_blank" class="course">
-                    <img src="${data.image}">
-                    <p><strong>${data.title}</strong></p>
-                    <p class="author">${data.author}</p>
-                    <div class="rate-section">
-                        ${rateHtmlFormatting(data.rate)}
-                    </div>
-                    <p class="price">E£${data.price}</p>
-                </a>
-            </li>\n`;
+    let html = `<div class="card-container">
+        <div class="course-item">
+            <a href="${data.link}" target="_blank">
+                <img src="${data.image}">
+                <p><strong>${data.title}</strong></p>
+                <p class="author">${data.author}</p>
+                <div class="rate-section">
+                    ${rateHtmlFormatting(data.rate)}
+                </div>
+                <p class="price">E£${data.price}</p>
+            </a>
+        </div>
+    </div>\n`;
+    return html;
+}
+
+const getCourses = (data, className) => {
+    let html = `<div class="${className} courses-list">\n`;
+    data.forEach((course) => {
+        html += getCourse(course);
+    });
+    html += `</div>\n`;
     return html;
 }
 
 window.onload = async () => {
     const data = await loadData();
     let html = ``;
-    for (let i = 0; data[i]; i++) 
-        html += getCourse(data[i]);
-
-    const coursesList = document.querySelector('.courses-list');
+    for (let ele in data) {
+        html += getCourses(data[ele], ele);
+    }
+    
+    const coursesList = document.querySelector('.carousel-inner');
     coursesList.insertAdjacentHTML('afterbegin', html);
 
+    let coursesDivs = document.querySelectorAll('.carousel-inner > div');
+    const divsArray = Array.from(coursesDivs);
+    divsArray.forEach((div) => {
+        div.style.display = "none";
+    });
+
+    // add tabs section
+    let tabs = document.querySelectorAll(".nav-item");
+    let tabsArray = Array.from(tabs);
+    tabsArray.forEach((tab) => {
+        if (tab.classList.contains("active")) {
+            document.querySelector(tab.dataset.cont).style.display = "flex";
+        }
+    });
+
+    tabsArray.forEach((tab) => {
+        tab.addEventListener("click", (e) => {
+            tabsArray.forEach((tab) => {
+                tab.classList.remove("active");
+                document.querySelector(tab.dataset.cont).style.display = "none";
+            });
+            e.currentTarget.classList.add("active");
+            document.querySelector(e.currentTarget.dataset.cont).style.display = "flex";
+        });
+    });
+
+    // search section
     let searchButton = document.getElementById('search-button');
     searchButton.addEventListener("click", (e) => {
         e.preventDefault();
         coursesList.innerHTML = '';
         const searchText = document.getElementById("input-bar").value.trim().toLowerCase();
-        html = ``;
-        for (let i = 0; data[i]; i++){
-            if(data[i].title.toLowerCase().includes(searchText))
-                html += getCourse(data[i]);
+        html = `<div class="courses-list">\n`;
+        for (let courses in data) {
+            data[courses].forEach((course) => {
+                if(course.title.toLowerCase().includes(searchText))
+                    html += getCourse(course);
+            });
         }
+        html += `</div>\n`;
         coursesList.insertAdjacentHTML('afterbegin', html);
     });
+
+    // slider section
+    const prevArrow = document.querySelector(".prev");
+    const nextArrow = document.querySelector(".next");
+    const coursesSection = document.querySelector('.courses-list');
+    const carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+    
+    let indexPage = 0;
+    prevArrow.addEventListener('click', () => {
+        indexPage--;
+        coursesSection.style.transform = `translateX(-${indexPage * carouselWidth}px)`;
+    });
+
+    nextArrow.addEventListener('click', () => {
+        indexPage++;
+        coursesSection.style.transform = `translateX(-${indexPage * carouselWidth}px)`;
+    });
+    
 };
